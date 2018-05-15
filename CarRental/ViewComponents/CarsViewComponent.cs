5cea35e8532421e80de97f499cb364b6cb1687a1
Valeri Bojarski <v.bojarski@hensel-recycling.com>
2018-05-15 09:51:41 +0200

    add ViewComponent

---
+++ CarRental/ViewComponents/CarsViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using CarRental.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRental.ViewComponents
+{
+    public class CarsViewComponent: ViewComponent
+    {
+        private CarRentalDBContext _ctx;
+
+        public CarsViewComponent(CarRentalDBContext ctx)
+        {
+            this._ctx = ctx;
+            if (_ctx.CarSet.Count() == 0)
+            {
+                _ctx.CarSet.Add(new Car { Id = 1, BrandName = "DMC", ModelName = "Delorian", YearOfConstruction = 1975 });
+                _ctx.SaveChanges();
+            }
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View( await _ctx.CarSet.ToListAsync());
+        }
+    }
+}
