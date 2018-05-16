a8e36092cd24d7c61df6c03efbac8b5f48ee08a6
Valeri Bojarski <v.bojarski@hensel-recycling.com>
2018-05-16 08:34:26 +0200

    add Xunit

---
+++ UnitTestController/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestController")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestController")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("34675c8f-62b2-4afb-82de-b60d522330a9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestController/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
+https://asp.net-hacker.rocks/2017/09/27/testing-aspnetcore.html
+namespace UnitTestController
+{
+    //[TestClass]
+    public class UnitTest1
+    {
+        //[TestMethlod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
