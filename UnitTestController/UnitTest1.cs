21ac26d718043aac78e7ad849574749fc2252af8
Valeri Bojarski <v.bojarski@hensel-recycling.com>
2018-05-16 08:49:55 +0200

    add XUnit

--- UnitTestController/UnitTest1.cs
+++ UnitTestController/UnitTest1.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 //using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Xunit;
-https://asp.net-hacker.rocks/2017/09/27/testing-aspnetcore.html
+//https://asp.net-hacker.rocks/2017/09/27/testing-aspnetcore.html
 namespace UnitTestController
 {
     //[TestClass]
